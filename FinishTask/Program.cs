f4eb4bba337d76fa542b0f7334d348963a6bb2f2
Zara Mirzakhmedova <zara-0512@yandex.ru>
2024-02-22 22:50:02 +0300

    add FinishTask

---
+++ FinishTask/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → [] */
+
+int CountArray(string[] inputArray)
+{
+    int count = 0;
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+string[] FilterArray(string[] inputArray)
+{
+    int count = CountArray(inputArray);
+    string[] resultArray = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            resultArray[index] = inputArray[i];
+            index++;
+        }
+    }
+    return resultArray;
+}
+
+string[] example = { "Hello", "2", "world", ":-)" };
+
+Console.WriteLine($"[\"{String.Join("\", \"", FilterArray(example))}\"]");
+
